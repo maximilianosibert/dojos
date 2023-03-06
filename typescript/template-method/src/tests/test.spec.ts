77c4ca91bfa032f54eb3651700f8a41870059dc9
msibert.ecl <maximiliano.sibert@eclypsium.com>
2023-03-06 14:26:44 -0300

    initial commit

---
+++ typescript/template-method/src/tests/test.spec.ts
@@ -0,0 +1,14 @@
+
+import { describe } from "mocha";
+import chaiAsPromised from 'chai-as-promised';
+import { assert } from "chai";
+var chai = require('chai');
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('Testing the configuration', () => {
+  it('should pass', () => {
+    
+  });
+});
