e85aee8b1fb89ebeddac1dd80d09bc6b87db96fd
msibert.ecl <maximiliano.sibert@eclypsium.com>
2023-03-07 11:13:15 -0300

    Extract methods

--- typescript/template-method/src/app/controller.ts
+++ typescript/template-method/src/app/controller.ts
@@ -1,30 +1,19 @@
-
 export class Controller{
 
     entryPointEven(a: number, b: number): string  {
 
-        if (a  === 0) {
-            throw new Error("The number must be greater than 0");
-        }
+        this.checkZeroParameter(a);
 
-        if (a % 2 != 0) {
-            throw new Error("The number must be even");
-        }
+        this.checkIsOdddParameter(a);
 
         const result = a * b;
 
-        if (result > 100) {
-            return "Invalid";
-        }
-
-        return "Valid";
-    }
+        return this.checkBigger100(result)
+    }    
 
     entryPointOdd(a: number, b: number): string  {
 
-        if (a  === 0) {
-            throw new Error("The number must be greater than 0");
-        }
+        this.checkZeroParameter(a);
 
         if (a % 2 === 0) {
             throw new Error("The number must be odd");
@@ -32,25 +21,43 @@ export class Controller{
 
         const result = a * b;
 
-        if (result > 100) {
+        return this.checkBigger100(result)
+    }
+
+    entryPointDivision(a: number, b: number): string  {
+
+        this.checkZeroParameter(b);
+
+        const result = a / b;
+
+        return this.checkIsZero(result);
+    }
+
+    private checkZeroParameter(a: number) {
+        if (a === 0) {
+            throw new Error("The number must be greater than 0");
+        }
+    }
+
+    private checkBigger100(a: number): string {
+        if (a > 100) {
             return "Invalid";
         }
 
         return "Valid";
     }
 
-    entryPointDivision(a: number, b: number): string  {
-
-        if (b  === 0) {
-            throw new Error("The number must be greater than 0");
-        }
-
-        const result = a / b;
-
-        if (result === 0) {
+    private checkIsZero(a: number): string {
+        if (a === 0) {
             return "Invalid";
         }
 
         return "Valid";
+    }
+
+    private checkIsOdddParameter(a: number) {
+        if (a % 2 != 0) {
+            throw new Error("The number must be even");
+        }
     }
 }
